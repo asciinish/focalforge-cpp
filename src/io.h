70f5bb82627ec07af26bd6ff8c47ecc7ec46bd67
asciinish <kaulnishit@gmail.com>
2026-05-18 11:13:06 +0530

    input_output_header_defined

---
+++ src/io.h
@@ -0,0 +1,12 @@
+#pragma once 
+#include <opencv2/core.hpp>
+#include <string>
+
+namespace io { 
+    
+cv::Mat load_image(const std::string& path);
+bool    save_image(const std::string& path, const cv::Mat& image);
+std::string promptLine(const std::string& message);
+
+    
+}
