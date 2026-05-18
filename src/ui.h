d8e8b1ee1d2e36ffc68bd144059cb49ef9a8ea16
asciinish <kaulnishit@gmail.com>
2026-05-18 11:44:23 +0530

    ui.h_final

--- src/ui.h
+++ src/ui.h
@@ -1,34 +1,43 @@
-#pragma once 
+#pragma once
 #include <opencv2/core.hpp>
 #include <deque>
 #include <string>
 
-namspace ui {
+namespace ui {
 
-struct Adjustments { 
-    int blurRadius = 0;
-    int brightness = 0;
-    int contrastX100 = 100;
-    int saturationX100 = 100;
-    int rotateAngle = 180;
-    int scaleX100 = 100;
-    bool negate = false;
+
+struct Adjustments {
+    int  blurRadius   = 0;     
+    int  brightness   = 0;     
+    int  contrastX100 = 100;   
+    int  saturationX100 = 100; 
+    int  rotateAngle  = 180;   
+    int  scaleX100    = 100;  
+    bool negate    = false;
     bool grayscale = false;
-    bool sepia = false;
+    bool sepia     = false;
 };
 
 struct EditorState {
-    cv::Mat base;
-    cv::Mat dispaly;
+    cv::Mat base;                      
+    cv::Mat display;                   
     Adjustments adj;
-    std::deque<cv::Mat> undo;
+    std::deque<cv::Mat> undo;           
     std::string windowName = "Image Editor";
-    bool dirty = true;
-    
-    // crop state
-    
+    bool dirty = true;                  
+
+    // Crop state
     bool cropMode = false;
     bool cropDragging = false;
     cv::Point cropStart;
     cv::Point cropEnd;
 };
+
+void  setupWindow(EditorState& st);
+void  pushUndo(EditorState& st);
+bool  popUndo(EditorState& st);
+
+// Called when state.dirty.
+cv::Mat renderDisplay(const EditorState& st);
+
+} 
